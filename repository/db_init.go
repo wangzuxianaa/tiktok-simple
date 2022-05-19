20110540718017f754f8b29b2b57c8fa634d0fe6
cyc <573553817@qq.com>
2022-05-19 20:59:10 +0800

    数据库初始化

---
+++ repository/db_init.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Init() (err error) {
+	dsn := "root:root@tcp(127.0.0.1:3306)/tiktok_simple?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	err = db.AutoMigrate(&User{})
+	return err
+}
