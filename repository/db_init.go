4f52c687afe6e96da5ff5c88e32ea6cac0751057
cyc <573553817@qq.com>
2022-05-22 12:56:13 +0800

    增加连接错误说明

--- repository/db_init.go
+++ repository/db_init.go
@@ -11,6 +11,7 @@ func Init() (err error) {
 	dsn := "root:root@tcp(127.0.0.1:3306)/tiktok_simple?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
+		panic("failed to connect database")
 		return err
 	}
 	err = db.AutoMigrate(&User{})
