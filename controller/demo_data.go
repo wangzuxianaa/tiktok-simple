2d2a20208235c6fcfc435e8909f94468d0cb8128
cyc <573553817@qq.com>
2022-05-28 20:23:50 +0800

    加了注释

--- controller/demo_data.go
+++ controller/demo_data.go
@@ -4,8 +4,8 @@ var DemoVideos = []Video{
 	{
 		Id:            1,
 		Author:        DemoUser,
-		PlayUrl:       "https://www.w3schools.com/html/movie.mp4",
-		CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
+		PlayUrl:       "http://192.168.43.254:8080/static/bear.mp4",
+		CoverUrl:      "",
 		FavoriteCount: 0,
 		CommentCount:  0,
 		IsFavorite:    false,
@@ -22,7 +22,7 @@ var DemoComments = []Comment{
 }
 
 var DemoUser = User{
-	Id:            1,
+	Id:            5,
 	Name:          "TestUser",
 	FollowCount:   0,
 	FollowerCount: 0,
