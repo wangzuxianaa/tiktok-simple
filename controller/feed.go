db3a80cbce6310d1767a115b9e1245c5bdb5cb40
YimingLi-orka <liyiming@hiorka.com>
2022-06-09 10:21:53 +0800

    更新了feed.go

--- controller/feed.go
+++ controller/feed.go
@@ -1,9 +1,11 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
+
+	"github.com/RaymondCode/simple-demo/repository"
+	"github.com/gin-gonic/gin"
 )
 
 type FeedResponse struct {
@@ -14,9 +16,42 @@ type FeedResponse struct {
 
 // Feed same demo video list for every request
 func Feed(c *gin.Context) {
+	var videoList []VideoMessage
+	videoRepo := repository.Video{}
+
+	videos, err := videoRepo.PullVideosFromServer()
+	if err != nil {
+		c.JSON(http.StatusOK, FeedResponse{
+			Response: Response{StatusCode: 1, StatusMsg: err.Error()},
+		})
+		return
+	}
+
+	for _, video := range videos {
+		videoMessage := VideoMessage{
+			Id: video.Id,
+			Author: UserMessage{
+				Id:            video.UserId,
+				Name:          video.Author.Username,
+				FollowCount:   video.Author.FollowCount,
+				FollowerCount: video.Author.FollowerCount,
+				IsFollow:      video.Author.IsFollow,
+			},
+			PlayUrl:       video.PlayUrl,
+			CoverUrl:      video.CoverUrl,
+			FavoriteCount: video.FavouriteCount,
+			CommentCount:  video.CommentCount,
+			IsFavorite:    video.IsFavourite,
+			Title:         video.Title,
+		}
+		videoList = append(videoList, videoMessage)
+	}
 	c.JSON(http.StatusOK, FeedResponse{
-		Response:  Response{StatusCode: 0},
-		VideoList: DemoVideos,
+		Response: Response{
+			StatusCode: 0,
+			StatusMsg:  "success",
+		},
+		VideoList: videoList,
 		NextTime:  time.Now().Unix(),
 	})
 }
